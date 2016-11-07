efaed793f8f4c0e9da4ab1022abc5cf437118a5c
Sophia Bok <sophiatca@gmail.com>
2016-11-07 18:24:07 -0500

    something undone

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var express = require('express');
+var nunjucks = require('nunjucks');
 var app = express();
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+debugger;
+nunjucks.configure('views',{ noCache: true });
+// nunjucks.render('index.html',locals, function (err,output){
+//  console.log(output);
+// });
+  nunjucks.render('index.html',locals, function (err,output){
+    res.send(output);
+  });
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
 
 app.use('/new',function(req,res,next){
   console.log(req.method+ req.originalUrl+" is-anybody-in-there");
@@ -12,7 +34,8 @@ app.get('/new', function(req, res){
   console.log(req.method+ req.originalUrl);
   //console.log(req.baseUrl);
   //console.log(req.path);
-  res.send("Hello World");
+
+  res.send("Hello World?");
 });
 
 app.post('/new', function(req,res){
